3428fac618d4a78c464fce8c4ae7a78cc824ecc3
marileemonmon <marilynsttan@gmail.com>
2026-06-11 14:57:59 +0800

    raycast collectible

--- Assets/Collectible.cs
+++ Assets/Collectible.cs
@@ -3,19 +3,11 @@ using UnityEngine;
 public class Collectible : MonoBehaviour
 {
     public int score=1;
-    public AudioSource audioSource;
+  
     public int Collect()
     {
-        //Play the coin collect sound
-        if (audioSource != null)
-        {
-            audioSource.Play();
-        }
-        //Hide the coin
-        var renderer = GetComponent<MeshRenderer>();
-        renderer.enabled = false;
         //Destroy the coin after the sound has played
-        Destroy(gameObject, 1);
+        Destroy(gameObject);
         return score;
     }
 }
--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -8,49 +8,27 @@ public class PlayerScript : MonoBehaviour
     [SerializeField]
     private float interactDistance = 3f;
     private GameObject currentgem;
+    public AudioSource audioSource;
 
     GameObject currentCollider;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-   /* void OnCollisionEnter(Collision collision)
+       void OnInteract (InputValue value)
     {
-        if(collision.gameObject.name.StartsWith("gem"))
-        {
-            currentCollider=collision.gameObject;
-            print($"Collided with {currentCollider.name}");
-        }
-    }
-    void OnCollisionExit(Collision collision)
-    {
-        if(collision.gameObject.name.StartsWith("gem"))
-        {
-            print($"Stopped colliding with {currentCollider.name}.");
-            currentCollider=null;
-        }
-    }*/
-    void OnInteract (InputValue value)
-    {
-        print($"Interacting with {currentCollider.name}");
-        /*if(currentCollider!=null)
-        {
-            var collectible = currentCollider.GetComponent<Collectible>();
-            if(collectible!=null)
-            {
-                print($"Interacting with {currentCollider.name}");
-                score+=collectible.score;
-                print($"Current score: {score}");
-                collectible.Collect();
-            }
-        }*/
+      
         if (currentgem!=null)
         {
             var collectible = currentgem.GetComponent<Collectible>();
-            if(collectible==currentgem.GetComponent<Collectible>())
-            {
+                
                 print($"Interacting with {currentgem.name}");
                 score+=collectible.score;
                 print($"Current score: {score}");
                 collectible.Collect();
-            }
+                //Play the coin collect sound
+                if (audioSource != null)
+                {
+                    audioSource.Play();
+                }
+
+        
         }
 
     }
